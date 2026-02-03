ee083fbbfd8719d4b7d7a39d8bcb412456364ea4
ngohoangloc <ngohoanglocit@gmail.com>
2026-02-03 11:09:25 +0700

    feat: add `SelectValue` component and export it from the select module.

---
+++ src/lib/components/ui/select/select-value.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Select as SelectPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+
+	type $$Props = SelectPrimitive.ValueProps;
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+</script>
+
+<SelectPrimitive.Value
+	class={cn('data-[placeholder]:text-muted-foreground', className)}
+	{...$$restProps}
+/>
